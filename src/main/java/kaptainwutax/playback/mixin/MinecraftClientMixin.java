23363688521fb9f2ed5d29d12a36d5b7e5de6410
Unknown <kaptainwutax@gmail.com>
2020-03-13 19:43:04 -0400

    wtf did i code there

--- src/main/java/kaptainwutax/playback/mixin/MinecraftClientMixin.java
+++ src/main/java/kaptainwutax/playback/mixin/MinecraftClientMixin.java
@@ -1,7 +1,6 @@
 package kaptainwutax.playback.mixin;
 
 import kaptainwutax.playback.Playback;
-import kaptainwutax.playback.capture.ReplayView;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
@@ -20,20 +19,7 @@ public class MinecraftClientMixin  {
 	@Inject(method = "tick", at = @At("HEAD"))
 	private void tick(CallbackInfo ci) {
 		if(this.world != null) {
-			if(Playback.isReplaying && Playback.manager.getView() == ReplayView.THIRD_PERSON && Playback.manager.replayPlayer != null) {
-				this.player = Playback.manager.replayPlayer.getPlayer();
-			}
-
 			Playback.update();
-		}
-	}
-
-	@Inject(method = "tick", at = @At("RETURN"))
-	private void tickReturn(CallbackInfo ci) {
-		if(this.world != null) {
-			if(Playback.isReplaying && Playback.manager.getView() == ReplayView.THIRD_PERSON && Playback.manager.replayPlayer != null) {
-				this.player = Playback.manager.cameraPlayer.getPlayer();
-			}
 		}
 	}
 
