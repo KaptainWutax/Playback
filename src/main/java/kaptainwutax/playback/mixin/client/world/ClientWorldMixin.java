dc40beb00fa984d4b670cfe13a48cd2b0e4fc566
2No2Name <2No2Name@web.de>
2020-04-01 17:49:32 +0200

    Fixed camera player appearing in chunk.getEntities

--- src/main/java/kaptainwutax/playback/mixin/client/world/ClientWorldMixin.java
+++ src/main/java/kaptainwutax/playback/mixin/client/world/ClientWorldMixin.java
@@ -1,11 +1,15 @@
 package kaptainwutax.playback.mixin.client.world;
 
 import kaptainwutax.playback.Playback;
+import kaptainwutax.playback.entity.FakePlayer;
 import kaptainwutax.playback.replay.recording.Recording;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.chunk.WorldChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.IOException;
@@ -25,6 +29,21 @@ public abstract class ClientWorldMixin {
 		}
 
 		Playback.getManager().restart();
+	}
+
+	/**
+	 * Prevent adding fake players to chunks, otherwise getEntities calls can find them, which for example leads to the
+	 * replay player's raycasts hitting a fake player or entities pushing themselves out of the fake players which will desync the replay.
+	 * @param worldChunk the chunk
+	 * @param entity the entity to be added
+	 */
+	@Redirect(method = "checkChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/WorldChunk;addEntity(Lnet/minecraft/entity/Entity;)V"))
+	private void addEntityIfNotFakePlayer(WorldChunk worldChunk, Entity entity) {
+		if (entity instanceof FakePlayer) {
+			entity.updateNeeded = true;
+			return;
+		}
+		worldChunk.addEntity(entity);
 	}
 
 }
