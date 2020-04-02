473ad1226f40c7d70a8640671aff143aade5a2ec
2No2Name <2No2Name@web.de>
2020-04-02 22:38:58 +0200

    Make elytra and water sounds only play when the replay player is displayed

--- src/main/java/kaptainwutax/playback/mixin/client/network/ClientPlayerEntityMixin.java
+++ src/main/java/kaptainwutax/playback/mixin/client/network/ClientPlayerEntityMixin.java
@@ -5,10 +5,14 @@ import kaptainwutax.playback.Playback;
 import kaptainwutax.playback.replay.ReplayView;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundManager;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
@@ -23,6 +27,25 @@ public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity
 		//usually returns true, but setting the camera on the cameraplayer shouldn't modify behavior (movement code), so still return true
 		if(Playback.getManager().replayPlayer != null && (Object) this == Playback.getManager().replayPlayer.getPlayer() && Playback.getManager().getView() == ReplayView.THIRD_PERSON) {
 			ci.setReturnValue(true);
+		}
+	}
+
+	/**
+	 * Cancel sounds that only play for the first person when in third person mode
+	 */
+	@Inject(method = "updateWaterSubmersionState", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;updateWaterSubmersionState()Z"), cancellable = true)
+	private void cancelWaterSoundsIfNotCameraPlayer(CallbackInfoReturnable<Boolean> cir) {
+		if (Playback.getManager().isReplaying() && (Playback.getManager().getView() == ReplayView.THIRD_PERSON) && ((PlayerEntity) this == Playback.getManager().replayPlayer.getPlayer())) {
+			cir.setReturnValue(this.isSubmergedInWater);
+		}
+	}
+	/**
+	 * Cancel sounds that only play for the first person when in third person mode
+	 */
+	@Redirect(method = "onTrackedDataSet", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundManager;play(Lnet/minecraft/client/sound/SoundInstance;)V"))
+	private void cancelElytraSoundsIfNotCameraPlayer(SoundManager soundManager, SoundInstance sound) {
+		if (!Playback.getManager().isReplaying() || (Playback.getManager().getView() != ReplayView.THIRD_PERSON) || ((PlayerEntity) this != Playback.getManager().replayPlayer.getPlayer())) {
+			soundManager.play(sound);
 		}
 	}
 
