19738ec478acae3c1b0341139fbb31fa0ae96569
2No2Name <2No2Name@web.de>
2020-04-19 19:12:39 +0200

    Added ReplayHud stub

---
+++ src/main/java/kaptainwutax/playback/gui/ReplayHud.java
@@ -0,0 +1,10 @@
+package kaptainwutax.playback.gui;
+
+import net.minecraft.client.gui.hud.InGameHud;
+
+public class ReplayHud {
+
+    private void renderReplayHud(InGameHud caller) {
+        //Todo
+    }
+}
