95c287ac034269bd6a32108f710a8379f80c9802
2No2Name <2No2Name@web.de>
2020-05-25 06:08:10 +0200

    fix: "38. The camera player having a different view distance than the replay player causes console being spammed with 'Changing view distance ...'"

--- src/main/java/kaptainwutax/playback/mixin/server/IntegratedServerMixin.java
+++ src/main/java/kaptainwutax/playback/mixin/server/IntegratedServerMixin.java
@@ -5,6 +5,7 @@ import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import com.mojang.datafixers.DataFixer;
 import kaptainwutax.playback.Playback;
+import kaptainwutax.playback.replay.PlayerFrame;
 import kaptainwutax.playback.replay.ReplayManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.WorldGenerationProgressListener;
@@ -35,5 +36,24 @@ public abstract class IntegratedServerMixin extends MinecraftServer {
     @Inject(method = "shutdown", at = @At("RETURN"))
     private void onShutdown(CallbackInfo ci) {
         Playback.getManager().setReplaying(ReplayManager.PlaybackState.NO_REPLAY);
+    }
+
+
+    /**
+     * Fix "38. the camera player having a different view distance than the replay player causes console being spammed with Changing view distance ...
+     *      - caused by Server Thread accessing this.client.options without any synchronization with the client"
+     * by always accessing the camera player's options.
+     * WARNING: this method is not reliable as the java memory model does NOT guarantee a lot without synchronization.
+     */
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Ljava/lang/Math;max(II)I"))
+    private int max(int min, int viewDistance) {
+        if (Playback.getManager().isInReplay()) {
+            //might also just return min instead, the integrated server doesn't load chunks anyways
+            PlayerFrame p = Playback.getManager().cameraPlayer;
+            if (p != null && p.options != null) {
+                viewDistance = -1 + p.options.getOptions().viewDistance;
+            }
+        }
+        return Math.max(min, viewDistance);
     }
 }
